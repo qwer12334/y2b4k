52966871a07c319a44a668195b3524968075f337
qwer12334 <59679467+qwer12334@users.noreply.github.com>
2020-02-08 18:24:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -187,12 +187,10 @@ $header='
         <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
     </head>
      <body style="background-color: #f8f8f8;">
-        <div class="w3-container w3-red w3-center" style="height:55px">
+       
            <img src="https://'.$_SERVER['SERVER_NAME'].'/2.png" alt="logo" style="height:35px;margin: 10px 0" />
         </div>';
-$footer='<footer class="w3-container w3-red w3-center" style="width: 100%;bottom: 0px;">
-            <p>©Development by <a href="https://2tube.js.org/" target="_blank">YOU2PHP</a></p>
-        </footer>
+
         </body>
 </html>';
 
